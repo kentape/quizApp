2108365970f888a1311dc0eef236d7e5e7661944
Kent Peterson <kenta_peterson@yahoo.se>
2021-01-03 23:58:07 +0100

    Build a Quiz App (6). Updated game.css, .html and .js in order to show current question in progress bar

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
-
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull")
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -55,7 +55,10 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText .innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText .innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
